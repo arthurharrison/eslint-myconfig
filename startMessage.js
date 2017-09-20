91f00cc252dcdcaa87f8a5b244149742e60ac5b4
Arthur Tavares <tavares593@gmail.com>
2017-09-20 03:02:03 -0300

    Learned a new use of npm and will be better developed in the future

---
+++ startMessage.js
@@ -0,0 +1,8 @@
+//const chalk = require('chalk'); //installing using npm install chalk
+//console.log(chalk.green('Starting Linting your Code...')); // using to display a Green message
+
+console.log('Starting Linting your code...');
+
+
+
+//Ps: This script only exists for learning purpose
